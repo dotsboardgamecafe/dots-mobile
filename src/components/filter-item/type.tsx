89879ffff273d1b9192614f45e0d5c5007c48a3d
agusw <agus.wardianzah@gmail.com>
2024-04-03 20:26:30 +0700

    slice find room

--- src/components/filter-item/type.tsx
+++ src/components/filter-item/type.tsx
@@ -1,7 +1,9 @@
 import type React from 'react'
+import { type StyleProp, type ViewStyle } from 'react-native'
 
 export interface FilterItemType {
   label: string,
+  style?: StyleProp<ViewStyle> | undefined,
   prefix?: React.ReactNode,
   suffix?: React.ReactNode,
   onPress?: () => void
---
+++ src/screen/play/data.ts
@@ -0,0 +1,41 @@
+export const sections = [
+	{
+		title: 'Tournaments',
+		data: [
+			{
+				img: 'https://picsum.photos/320/140',
+				type: 'tourney'
+			}
+		]
+	},
+	{
+		title: 'Special Event',
+		data: [
+			{
+				img: 'https://picsum.photos/320/140',
+				type: 'event'
+			},
+			{
+				img: 'https://picsum.photos/320/140',
+				type: 'event'
+			},
+		]
+	},
+	{
+		title: 'Game Room',
+		data: [
+			{
+				img: 'https://picsum.photos/320/140',
+				type: 'room'
+			},
+			{
+				img: 'https://picsum.photos/320/140',
+				type: 'room'
+			},
+			{
+				img: 'https://picsum.photos/320/140',
+				type: 'room'
+			},
+		]
+	},
+]
