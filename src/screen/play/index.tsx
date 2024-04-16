91cdb822db8575a951cea248b0efae9e479f86aa
agusw <agus.wardianzah@gmail.com>
2024-04-17 00:12:40 +0700

    integrate api

--- src/screen/play/index.tsx
+++ src/screen/play/index.tsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { SectionList, type SectionListData, TouchableOpacity, View } from 'react-native'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
+
 import { type NavigationProps } from '../../models/navigation'
 import Container from '../../components/container'
 import withCommon from '../../hoc/with-common'
 import FilterItem from '../../components/filter-item'
 import { ArrowDown2, Location } from 'iconsax-react-native'
 import { scaleVertical, scaleWidth } from '../../utils/pixel.ratio'
-import { Image, SectionList, TouchableOpacity, View } from 'react-native'
-import { sections } from './data'
 import Text from '../../components/text'
 import styles from './styles'
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
+import { useGetListRoomQuery } from '../../store/room'
+import { type Rooms } from '../../models/rooms'
+import Image from '../../components/image'
 
 type Props = NavigationProps<'play'>
 
-const Play = ({ theme, navigation }: Props): React.ReactNode => {
+interface Sections { title: string, data: Array<Partial<Rooms>> }
+
+const Play = ({ theme, navigation, t }: Props): React.ReactNode => {
 	const tabBarHeight = useBottomTabBarHeight()
+	const { data } = useGetListRoomQuery()
+	const [sections, setSections] = useState<Sections[]>([])
+
+	const sectionFooter = useCallback((info: {section: SectionListData<Partial<Rooms>, Sections>}) => {
+		return (
+			<View style={ styles.sectionFooter }>
+				{ !info.section.data.length && <Text variant='bodySmallMedium'>Data not found</Text> }
+			</View>
+		)
+	}, [])
+
+	useEffect(() => {
+		const raw = data?.map((r: Rooms) => ({
+			room_code: r.room_code,
+			room_img_url: r.room_img_url,
+			room_type: r.room_type
+		})) ?? []
+		setSections([
+			{ title: t('play-page.tournament'), data: raw.filter(r => r.room_type === 'tournament') },
+			{ title: t('play-page.special-event'), data: raw.filter(r => r.room_type === 'special_event') },
+			{ title: t('play-page.game-room'), data: raw.filter(r => r.room_type === 'normal') },
+		])
+	}, [data])
+
 	return (
 		<Container
-			contentStyle={ { paddingVertical: scaleVertical(16) } }
+			contentStyle={ styles.content }
 		>
 			<FilterItem
 				label='All location'
@@ -26,7 +55,7 @@ const Play = ({ theme, navigation }: Props): React.ReactNode => {
 						size={ scaleWidth(14) }
 						variant='Bold'
 						color={ theme.colors.onBackground }
-						style={ { marginEnd: 4 } }
+						style={ styles.filterPrefix }
 					/>
 				}
 				suffix={
@@ -34,22 +63,21 @@ const Play = ({ theme, navigation }: Props): React.ReactNode => {
 						variant='Linear'
 						color={ theme.colors.onBackground }
 						size={ 14 }
-						style={ { marginStart: 4 } }
+						style={ styles.filterSuffix }
 					/>
 				}
 				style={ [styles.filter, styles.mh] }
 			/>
-
 			<SectionList
 				sections={ sections }
-				keyExtractor={ (item, index) => item.type + index }
+				keyExtractor={ (item, index) => (item.room_code ?? '') + index }
 				renderItem={ ({ item }) => (
 					<TouchableOpacity onPress={ () => {
-						navigation.navigate('roomDetail', { type: item.type })
+						navigation.navigate('roomDetail', item)
 					} }
 					>
 						<Image
-							source={ { uri: item.img } }
+							source={ { uri: item.room_img_url } }
 							style={ styles.item }
 						/>
 					</TouchableOpacity>
@@ -57,7 +85,7 @@ const Play = ({ theme, navigation }: Props): React.ReactNode => {
 				renderSectionHeader={ ({ section: { title } }) => (
 					<Text variant='bodyExtraLargeHeavy' style={ styles.section }>{ title }</Text>
 				) }
-				renderSectionFooter={ () => <View style={ styles.sectionFooter } /> }
+				renderSectionFooter={ sectionFooter }
 				ItemSeparatorComponent={ () => <View style={ styles.itemSeparator } /> }
 				contentContainerStyle={ { paddingBottom: tabBarHeight } }
 				style={ { marginTop: scaleVertical(8) } }
